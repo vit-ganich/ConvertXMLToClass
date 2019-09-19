54eaf53d3c8298c8bd45ff73570826ccc6b7d8e5
vhanich <vitali.hanich@gmail.com>
2019-09-19 09:44:12 +0300

    Initial commit

---
+++ ConvertXMLToClass/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace Sandbox
+{
+    /// <summary>
+    /// Find a nessesary file, save it in convinient place and change PathToXMLFile variable.
+    /// Run and copy text from the command line window.
+    /// </summary>
+    class Program
+    {
+        public const string PathToXMLfile = @"D:\Prototypes\Sandbox\Sandbox\Manage P~E.xml";
+
+        static void Main(string[] args)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Repository));
+
+            FileStream fs = new FileStream(PathToXMLfile, FileMode.Open);
+
+            Repository repository = serializer.Deserialize(fs) as Repository;
+
+            // Define a class for Page Object
+            Console.WriteLine(string.Format("public class {0}Page {{", repository.Container.Name));
+
+            foreach (var item in repository.Container.Object)
+            {
+                string locator = "";
+                if (!string.IsNullOrEmpty(item.Id))
+                    locator = string.Format("By.Id(\"{0}\")", item.Id);
+                else if (!string.IsNullOrEmpty(item.Name))
+                    locator = string.Format("By.Name(\"{0}\")", item.Name);
+                else if(!string.IsNullOrEmpty(item.XPath))
+                    locator = string.Format("By.XPath(\"{0}\")", item.XPath);
+                else
+                    locator = string.Format("By.XPath(\"//*[normalize-space(text())='{0}']\")", item.Label);
+
+                string labelName = string.Join("", item.Label.Split(' '));
+
+                if (item.Type == null)
+                    item.Type = "UnknownControl";
+
+                string result = string.Format("public {0} {1} => new {0} ({2}, this);", item.Type, labelName, locator);
+
+                Console.WriteLine(result);
+            }
+            Console.WriteLine("}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConvertXMLToClass/Serialization.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Sandbox
+{
+    [XmlRoot(ElementName = "Object")]
+    public class Object
+    {
+        [XmlElement(ElementName = "label")]
+        public string Label { get; set; }
+        [XmlElement(ElementName = "type")]
+        public string Type { get; set; }
+        [XmlElement(ElementName = "id")]
+        public string Id { get; set; }
+        [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+        [XmlElement(ElementName = "xpath")]
+        public string XPath { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Container")]
+    public class Container
+    {
+        [XmlElement(ElementName = "label")]
+        public string Label { get; set; }
+        [XmlElement(ElementName = "type")]
+        public string Type { get; set; }
+        [XmlElement(ElementName = "Object")]
+        public List<Object> Object { get; set; }
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Repository")]
+    public class Repository
+    {
+        [XmlElement(ElementName = "Container")]
+        public Container Container { get; set; }
+    }
+}
